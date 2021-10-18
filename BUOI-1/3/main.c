81264aa5bbcbf1d099d712d8f307213a9c4d52bb
anhkhoa1603 <anhkhoa162003@gmail.com>
2021-10-18 23:02:01 +0700

    [fix] Dat lai ten ham

--- BUOI-1/3/main.c
+++ BUOI-1/3/main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <math.h>
 
-float cong_thuc_heron(float a, float b, float c)
+float congThucHeron(float a, float b, float c)
 {
     float p = (a + b + c) / 2.0;
     return sqrt(p * (p - a) * (p - b) * (p - c));
 }
-void Check(float a, float b, float c)
+// Ham check dieu kien de la 1 tam giac
+void checkTamGiac(float a, float b, float c)
 {
     if (a + b > c && a + c > b && b + c > a)
     {
@@ -22,7 +23,7 @@ int main()
     printf("nhap 3 canh cua tam giac: ");
     float a, b, c;
     scanf("%f%f%f", &a, &b, &c);
-    printf("Dien tich tam giac = %.2f\n", cong_thuc_heron(a, b, c));
-    Check(a, b, c);
+    printf("Dien tich tam giac = %.2f\n", congThucHeron(a, b, c));
+    checkTamGiac(a, b, c);
     return 0;
 }
