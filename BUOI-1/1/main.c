86f976d129980e44d73dfe052d1f1c8e5ac99ca6
anhkhoa1603 <anhkhoa162003@gmail.com>
2021-10-21 13:25:58 +0700

    [M] chinh sua

--- BUOI-1/1/main.c
+++ BUOI-1/1/main.c
@@ -2,23 +2,23 @@
 
 void ham(char letter)
 {
-   int a;
+    int a;
 
-   printf("nhap %c:", letter);
-   scanf("%d",&a);
+    printf("nhap %c:", letter);
+    scanf("%d", &a);
 
-   printf("%c = %d\n",letter,a);
-   printf("%c^2 = %d\n",letter,a*a);
-   printf("%c^3 = %d\n",letter,a*a*a);
+    printf("%c = %d\n", letter, a);
+    printf("%c^2 = %d\n", letter, a * a);
+    printf("%c^3 = %d\n", letter, a * a * a);
 }
 
-float trung_binh_cong(float a,float b,float c)
+float trung_binh_cong(float a, float b, float c)
 {
-    float sum = a+b+c;
-    float avg = sum/3.0;
+    float sum = a + b + c;
+    float avg = sum / 3.0;
 
-    a = a*100;
-    printf("a inside funcsion: %f\n",a);
+    a = a * 100;
+    printf("a inside funcsion: %f\n", a);
 
     return avg;
 }
@@ -29,9 +29,9 @@ int main()
     float y = 7.0;
     float z = 9.0;
 
-    printf("Diem TB = %.2f", trung_binh_cong(x,y,z));
+    printf("Diem TB = %.2f", trung_binh_cong(x, y, z));
 
-    printf("\nX = %.2f",x);
+    printf("\nX = %.2f", x);
 
     return 0;
 }
