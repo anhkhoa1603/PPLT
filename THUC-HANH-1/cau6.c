9257c85dd4c169558854b01a6136c73ba354165e
anhkhoa1603 <anhkhoa162003@gmail.com>
2021-10-28 14:03:52 +0700

    [+] Cau 6

---
+++ THUC-HANH-1/cau6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int a;
+    printf("Nhap so a: ");
+    scanf("%d", &a);
+    
+    printf("Gia tri tuyet doi cua a = %d", abs(a));
+    
+    return 0;
+}
