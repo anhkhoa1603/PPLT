fe96f90dac823f30377554d7b09909c7fa49d76b
anhkhoa1603 <anhkhoa162003@gmail.com>
2021-10-28 14:10:01 +0700

    [+] Cau 7

---
+++ THUC-HANH-1/cau7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int year;
+    printf("Nhap nam: ");
+    scanf("%d", &year);
+
+    if (year % 4 == 0)
+    {
+        printf("Nam %d la nam nhuan!", year);
+    }
+    else
+    {
+        printf("Nam %d khong la nam nhuan!", year);
+    }
+
+    return 0;
+}
