dbac6fc9f27c44fb1e3292d0ff17a7ef5cb1714e
anhkhoa1603 <anhkhoa162003@gmail.com>
2021-10-28 14:25:50 +0700

    [+] Cau 9

---
+++ THUC-HANH-1/cau9.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    printf("Nhap 3 so a, b ,c : ");
+    scanf("%d%d%d", &a, &b, &c);
+
+    if ( a> b)
+    {
+        if (a>c)
+        {
+            printf("Max = %d", a);
+        }
+        else
+        {
+            printf("Max = %d", c);
+        }
+    }
+    else
+    {
+        if (b>c)
+        {
+            printf("Max = %d", b);
+        }
+        else
+        {
+            printf("Max = %d", c);
+        }
+    }
+
+    return 0;
+}
