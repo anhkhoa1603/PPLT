3ebbb29c137b67d8ea5fb3a52ba929eba26ec85e
anhkhoa1603 <anhkhoa162003@gmail.com>
2021-11-04 13:11:10 +0700

    [+] Cau9

---
+++ THUC-HANH-2/cau9.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Moi nhap so N > 10 : ");
+    scanf("%d", &n);
+
+    int x = n % 10;
+    n = n / 10;
+    int dem = 1, cong = 0, y;
+
+    while (n > 10)
+    {
+        y = n % 10;
+        n = n / 10;
+        dem = dem * 10;
+        cong = cong + y * dem;
+    }
+    
+    int m = (x * dem * 10) + cong + n;
+    printf("So sau khi doi 2 vi tri: %d", m);
+}
