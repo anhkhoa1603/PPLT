87d537adaf6f2d7c2ebfff984ef4ddf05f3b057e
anhkhoa1603 <anhkhoa162003@gmail.com>
2021-11-18 14:07:03 +0700

    [+] Cau 5

---
+++ THUC-HANH-4/cau5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int tong(int a[], int n)
+{
+    int  sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] < 0)
+        {
+            sum += a[i];
+        }
+        
+        
+    }
+
+    return sum;
+}
+
+void nhapMang(int a[], int *n)
+{
+    printf("Nhap so luong phan tu: ");
+    scanf("%d", n);
+
+    for (int i = 0; i < *n; i++)
+    {
+        printf("a[%d]: ", i);
+        scanf("%d", &a[i]);
+    }
+}
+
+int main()
+{
+    int a[1000], n;
+    
+    nhapMang(a, &n);
+
+    int sum = tong(a, n);
+    printf("Max = %d", sum);
+
+    return 0;
+    
+}
